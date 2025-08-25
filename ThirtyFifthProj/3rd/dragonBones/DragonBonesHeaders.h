cd7165059ed081c7bff393fc5b6de7df82919408
NilNull818 <diaosi.dev@gmail.com>
2025-08-25 17:08:30 +0800

    add dragonbones sfml demo

---
+++ ThirtyFifthProj/3rd/dragonBones/DragonBonesHeaders.h
@@ -0,0 +1,50 @@
+#ifndef DRAGONBONES_HEADERS_H
+#define DRAGONBONES_HEADERS_H
+
+// core
+#include "dragonBones/DragonBones.h"
+#include "dragonBones/BaseObject.h"
+
+// geom
+#include "dragonBones/ColorTransform.h"
+#include "dragonBones/Matrix.h"
+#include "dragonBones/Point.h"
+#include "dragonBones/Rectangle.h"
+#include "dragonBones/Transform.h"
+
+// textures
+#include "dragonBones/TextureData.h"
+
+// model
+#include "dragonBones/FrameData.h"
+#include "dragonBones/DragonBonesData.h"
+#include "dragonBones/ArmatureData.h"
+#include "dragonBones/AnimationData.h"
+#include "dragonBones/TimelineData.h"
+
+// parsers
+#include "dragonBones/DataParser.h"
+#include "dragonBones/JSONDataParser.h"
+
+// factories
+#include "dragonBones/BaseFactory.h"
+
+// armature
+#include "dragonBones/IArmatureDisplay.h"
+#include "dragonBones/Armature.h"
+#include "dragonBones/TransformObject.h"
+#include "dragonBones/Bone.h"
+#include "dragonBones/Slot.h"
+
+// animation
+#include "dragonBones/IAnimateble.h"
+#include "dragonBones/WorldClock.h"
+#include "dragonBones/Animation.h"
+#include "dragonBones/AnimationState.h"
+#include "dragonBones/BaseTimelineState.h"
+#include "dragonBones/TimelineState.h"
+
+// events
+#include "dragonBones/EventObject.h"
+
+#endif // DRAGONBONES_HEADERS_H
